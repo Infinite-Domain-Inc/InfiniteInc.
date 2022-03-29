d297b651e2ba952e57003f06749e65608b8852e5
Divyam Samarwal <divyamsamarwal@gmail.com>
2022-03-29 17:17:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to Infinite Domain Inc. New Delhi, India
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/Infinite-Domain-Inc"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit Github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
